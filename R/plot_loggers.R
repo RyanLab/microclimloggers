2a2f6f907b3517688ee3a72afe7d91f6c8c9e285
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-08-17 17:10:47 -0400

    fixed S3 consistency issue in plot method

--- R/plot_loggers.R
+++ R/plot_loggers.R
@@ -1,15 +1,16 @@
 #' Plot microclimate logger output
 #'
 #' @param x a microclim object
+#' @param ... further arguments to plot
 #'
 #' @return a plot
 #' @export
-plot.microclim <- function(x){
+plot.microclim <- function(x, ...){
   # store old par
   old.par <- par(no.readonly=TRUE)
   par(mfrow=c(2,1))
-  plot(x$df_env$Timestamp, x$df_env$Temp.C, type = "l", col='red', xlab = "Time", ylab= "Temp", main = unique(x$df_env$Logger.SN))
-  plot(x$df_env$Timestamp, x$df_env$RH.perc, type = "l", col='blue', xlab = "Time", ylab="RH")
+  plot(x$df_env$Timestamp, x$df_env$Temp.C, type = "l", col='red', xlab = "Time", ylab= "Temp", main = unique(x$df_env$Logger.SN), ...)
+  plot(x$df_env$Timestamp, x$df_env$RH.perc, type = "l", col='blue', xlab = "Time", ylab="RH", ...)
   # restore old par
   par(old.par)
 }
