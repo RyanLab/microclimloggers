140843ee1e832294eef5a6a94600baf17b5ddbc5
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-08-17 14:45:17 -0400

    added simple plotting method, static handling of variables only

---
+++ R/plot_loggers.R
@@ -0,0 +1,15 @@
+#' Plot microclimate logger output
+#'
+#' @param x a microclim object
+#'
+#' @return a plot
+#' @export
+plot.microclim <- function(x){
+  # store old par
+  old.par <- par(no.readonly=TRUE)
+  par(mfrow=c(2,1))
+  plot(x$df_env$Timestamp, x$df_env$Temp.C, type = "l", col='red', xlab = "Time", ylab= "Temp")
+  plot(x$df_env$Timestamp, x$df_env$RH.perc, type = "l", col='blue', xlab = "Time", ylab="RH")
+  # restore old par
+  par(old.par)
+}
